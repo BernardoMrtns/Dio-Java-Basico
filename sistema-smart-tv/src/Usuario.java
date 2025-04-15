461a778f43460eb34467a680c09bd4a215e8c93b
Bernardo <bernardomartinsbr@gmail.com>
2025-04-15 13:00:15 -0300

    adicao de exercicio TvSmart

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario {
+    public static void main(String[] args) {
+
+        SmartTv SmartTv = new SmartTv();
+    
+        System.out.println("Tv ligada? " + SmartTv.ligada);
+        System.out.println("Canal Atual: " + SmartTv.canal);
+        System.out.println("Volume Atual: " + SmartTv.volume);   
+
+        SmartTv.ligar();
+        System.out.println("Novo Status -> Tv Ligada? " + SmartTv.ligada);
+        SmartTv.desligar();
+        System.out.println("Novo Status -> Tv Ligada? " + SmartTv.ligada);
+
+        SmartTv.aumentarVolume();
+        System.out.println("Novo Status -> Volume da TV " + SmartTv.volume);
+        SmartTv.diminuirVolime();
+        System.out.println("Novo Status -> Volume da TV " + SmartTv.volume);
+
+        SmartTv.aumentarCanal();
+        System.out.println("Novo Status -> Canal da TV " + SmartTv.canal);
+        SmartTv.diminuirCanal();
+        System.out.println("Novo Status -> Canal da TV " + SmartTv.canal);
+
+    }
+}
